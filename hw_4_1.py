6c3699711804f3a926b00a01e436b53c269655da
MykolaKarabazhak <47992425+MykolaKarabazhak@users.noreply.github.com>
2019-02-26 12:53:33 +0200

    Add files via upload

---
+++ hw_4_1.py
@@ -0,0 +1,22 @@
+enter_user = [input("Введите строку или текст для пощёта сиволов ")]
+for i in enter_user:
+     print(i, end="n\n ")
+print(enter_user)
+isldfhkjs
+ sum(fsd
+     fordsg
+dfg
+fds
+ds
+hasattr(gd
+        copyright()sd
+fgds
+fg
+dsg
+ds
+getattr(dsf
+        gds
+getattr(df
+        getattr(dg
+                ds
+getattr(dsfgsd))))))
---
+++ index_git.py
@@ -0,0 +1,6 @@
+a = 5
+print(a)
+if a > 0:
+    for i in range(a):
+        print(i)
+        i -= 1
